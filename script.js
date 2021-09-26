b7356b730ec882053b822d679b22645f42cd157c
ccvanginkel <34521994+ccvanginkel@users.noreply.github.com>
2021-09-26 15:44:03 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,38 @@
+const age = 18;
+var invoer = 18;
+
+if (invoer >= age) {
+
+    console.log("Je mag naar binnen");
+
+} else if (invoer < age) {
+
+    console.log("Je mag niet naar binnen");
+
+}
+
+const isFemale = true;
+var invoerGeslacht = isFemale;
+
+if (invoerGeslacht = isFemale) {
+
+    console.log("Je mag naar binnen");
+
+} else {
+
+    console.log("Je mag niet naar binnen");
+
+}
+
+const driverStatus = 'bob';
+var invoerDrinken = true;
+
+if (invoerDrinken) {
+
+    console.log("U heeft de status van",driverStatus,"en daarom mag u rijden");
+
+} else {
+
+    console.log("U heeft niet de status van",driverStatus,"en u mag daarom niet rijden");
+
+}
